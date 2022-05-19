84b51eb560d95d82471d029810de6dc08dcb9d12
Bartolome Peirano <bpeirano1@uc.cl>
2022-05-19 15:31:06 -0400

    creating enpoints to each category, product and all the products

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import DummyView
+from .views import DummyView, ProductsView, CategoryView,ProductView
 
 urlpatterns = [
     path("dummy/", DummyView.as_view()),
+    path("v1/products/", ProductsView.as_view()),
+    path("v1/products/<str:category>", CategoryView.as_view()),
+    path("v1/product/<int:product_id>", ProductView.as_view()),
 ]
