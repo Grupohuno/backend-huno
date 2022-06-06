e280b184b9f2c340232564e7a06221eda7237782
Bartolome Peirano <bpeirano1@uc.cl>
2022-06-06 18:38:30 -0400

    refactor: Using Black formatter

--- store/serializers.py
+++ store/serializers.py
@@ -23,15 +23,15 @@ class CategoryModelSerializer(serializers.ModelSerializer):
         model = Category
         fields = "__all__"
 
-class ProductResponseSerializer(serializers.Serializer):
-    id=serializers.IntegerField()
-    name =  serializers.CharField(max_length=180)
-    store= serializers.CharField(max_length=180)
-    category= serializers.CharField(max_length=180)
-    brand=serializers.CharField(max_length=180)
-    size=serializers.CharField(max_length=180)
-    image=serializers.CharField(max_length=280)
-    redirect_page=serializers.CharField(max_length=280)
-    price=serializers.IntegerField()
-    is_promotion=serializers.BooleanField()
 
+class ProductResponseSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=180)
+    store = serializers.CharField(max_length=180)
+    category = serializers.CharField(max_length=180)
+    brand = serializers.CharField(max_length=180)
+    size = serializers.CharField(max_length=180)
+    image = serializers.CharField(max_length=280)
+    redirect_page = serializers.CharField(max_length=280)
+    price = serializers.IntegerField()
+    is_promotion = serializers.BooleanField()
--- store/services.py
+++ store/services.py
@@ -1,28 +1,13 @@
-from store.models import Price, Product,Store, Category
+from store.models import Price, Product, Store, Category
 from rest_framework.response import Response
 from rest_framework import status
 from .utils import get_time
 
-def build_obj_list(queryset):
-    obj_list =[]
-    for product in queryset:
-            product_obj = {
-                "id": product.id,
-                "name": product.name,
-                "store": product.store_id.name,
-                "category": product.category_id.name,
-                "brand": product.brand,
-                "size": product.size,
-                "image": product.image_url,
-                "redirect_page": product.page_url,
-                "price": Price.objects.filter(product_id=product).last().price,
-                "is_promotion": product.is_promotion,
-            }
-            obj_list.append(product_obj)
-    return obj_list
 
-def build_obj(product):
-    product_obj = {
+def build_obj_list(queryset):
+    obj_list = []
+    for product in queryset:
+        product_obj = {
             "id": product.id,
             "name": product.name,
             "store": product.store_id.name,
@@ -34,13 +19,30 @@ def build_obj(product):
             "price": Price.objects.filter(product_id=product).last().price,
             "is_promotion": product.is_promotion,
         }
+        obj_list.append(product_obj)
+    return obj_list
+
+
+def build_obj(product):
+    product_obj = {
+        "id": product.id,
+        "name": product.name,
+        "store": product.store_id.name,
+        "category": product.category_id.name,
+        "brand": product.brand,
+        "size": product.size,
+        "image": product.image_url,
+        "redirect_page": product.page_url,
+        "price": Price.objects.filter(product_id=product).last().price,
+        "is_promotion": product.is_promotion,
+    }
     return product_obj
 
 
 def validate_and_save_data(request):
     try:
-            store_name = request.data["store"].title()
-            products_list = request.data["products_list"]
+        store_name = request.data["store"].title()
+        products_list = request.data["products_list"]
     except Exception:
         return Response({"message": "Invalid data"}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -53,9 +55,7 @@ def validate_and_save_data(request):
         try:
             category = Category.objects.get(name=product["category"].title())
         except Exception:
-            return Response(
-                {"message": "Error in Category", "product": product}, status=status.HTTP_400_BAD_REQUEST
-            )
+            return Response({"message": "Error in Category", "product": product}, status=status.HTTP_400_BAD_REQUEST)
         try:
             product_obj = Product.objects.get(sku=product["sku"])
         except Product.DoesNotExist:
@@ -68,7 +68,7 @@ def validate_and_save_data(request):
                 size=product["size"],
                 image_url=product["image_url"],
                 page_url=product["page_url"],
-                is_promotion = product["is_promotion"],
+                is_promotion=product["is_promotion"],
             )
         time_now = get_time()
         Price.objects.create(price=product["price"], date=time_now, product_id=product_obj)
